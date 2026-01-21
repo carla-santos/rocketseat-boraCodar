f5d14971c54e541b2bc671cd3d2a736a0fafb922
Carla Santos <carla.devjs@gmail.com>
2026-01-21 17:18:31 -0300

    Atualiza card dos desafios

--- js/main.js
+++ js/main.js
@@ -8,37 +8,38 @@ function updateView() {
     .map((desafio) => {
       const { title, subtitle, image, alt, repoLink, liveLink } = desafio;
 
+      const card = document.createElement('div');
+      card.classList.add('card group opacity-0 translate-y-8');
+
       return `
-      <div class="card-image-container">
-        <img src="${image}" alt="${alt}">
-        <div class="absolute inset-0 bg-purple-900/30 group-hover:bg-transparent transition-all duration-300"></div>
-      </div>
+        <div class="card-image-container">
+          <img src="${image}" alt="${alt}">
+          <div class="absolute inset-0 bg-purple-900/30 group-hover:bg-transparent transition-all duration-300"></div>
+        </div>
 
-      <div class="card-content">
-        <h2 class="text-xl md:text-2xl text-cyan-400 mb-2 retro-font group-hover:text-pink-500 transition-colors truncate">
-          ${title}
-        </h2>
+        <div class="card-content">
+          <h2 class="text-xl md:text-2xl text-cyan-400 mb-2 retro-font group-hover:text-pink-500 transition-colors truncate">
+            ${title}
+          </h2>
         
-        <p class="text-gray-400 text-base md:text-lg mb-4 font-sans leading-tight">
-          ${subtitle}
-        </p>
+          <p class="text-gray-400 text-base md:text-lg mb-4 font-sans leading-tight">
+            ${subtitle}
+          </p>
 
-        <div class="flex items-center mb-6 border-t border-gray-800 pt-4 text-gray-300">
-          
-        </div>
+          <div class="flex items-center mb-6 border-t border-gray-800 pt-4 text-gray-300"></div>
 
-        <div class="flex gap-4">
-          <a href="${repoLink}" target="_blank" class="btn-retro flex-1 justify-center text-sm md:text-base">
-            <i class="fab fa-github"></i> Repo
-          </a>
-          
-          <a href="${liveLink}" target="_blank"
-            class="btn-retro btn-retro-secondary flex-1 justify-center text-sm md:text-base">
-            <i class="fas fa-external-link-alt"></i> Demo
-          </a>
+          <div class="flex gap-4">
+            <a href="${repoLink}" target="_blank" class="btn-retro flex-1 justify-center text-sm md:text-base">
+              <i class="fab fa-github"></i> Repo
+            </a>
+            
+            <a href="${liveLink}" target="_blank"
+              class="btn-retro btn-retro-secondary flex-1 justify-center text-sm md:text-base">
+              <i class="fas fa-external-link-alt"></i> Demo
+            </a>
+          </div>
         </div>
-      </div>
-    `;
+      `;
     })
     .join('');
 
