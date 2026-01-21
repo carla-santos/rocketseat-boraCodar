33a7962fe251aab781731b7d11a0381d9646d85a
Carla Santos <carla.devjs@gmail.com>
2026-01-21 15:05:16 -0300

    Adiciona icones

---
+++ js/config/icons.js
@@ -0,0 +1,5 @@
+export const techIconsMap = {
+  html: '<i class="fab fa-html5" title="HTML5"></i>',
+  css: '<i class="fab fa-css3-alt" title="CSS3"></i>',
+  js: '<i class="fab fa-js" title="JavaScript"></i>',
+};
